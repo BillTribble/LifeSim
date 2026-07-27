e819f1c69b3c317c40a60ef3d4e0f6c273f70f38
Bill Tribble <tribble@google.com>
2026-07-27 19:12:45 +0100

    Remove legacy fetch('/api/log') POST request to fix HTTP 405 Method Not Allowed console errors

--- src/App.tsx
+++ src/App.tsx
@@ -167,7 +167,7 @@ export default function App() {
         stats={stats}
         onLog={(msg) => {
           console.log(msg);
-          fetch('/api/log', { method: 'POST', body: msg }).catch(() => {});
+          handleLog(msg);
         }}
         onStateUpdate={handleStateUpdate}
         onConfigChange={(config) => {
