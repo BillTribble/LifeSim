f579c2adaae68539ea17eb39deac31212e0d413b
Bill Tribble <tribble@google.com>
2026-07-27 23:15:10 +0100

    Update default appendage size (flowerSize) to 1.2

--- src/lib/SimulationEngine.ts
+++ src/lib/SimulationEngine.ts
@@ -93,7 +93,7 @@ export class SimulationEngine {
   proximity: number = 400.0;
   desperation: number = 2.0;
   despairAge: number = 1000;
-  flowerSize: number = 3.0;
+  flowerSize: number = 1.2;
   entropyThreshold: number = 0.0;
   globalPulseSpeed: number = 1.0;
   maxLineWidth: number = 8.0;
